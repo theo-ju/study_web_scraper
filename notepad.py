642d6269b2e2f908e940bd39633989b000806da5
theo-ju <theo.ju@outlook.com>
2021-09-28 18:56:41 +0900

    feat: Create index.html

--- notepad.py
+++ notepad.py
@@ -1,39 +1,10 @@
-lst_days = ['mon', 'tue', 'wed', 'thu']
-tup_days = ('mon', 'tue')
-dict_t = {
-    'name': 'tae-hoon',
-    'age': 34,
-    'korean': True,
-    'fav_foods': ['kimchi', 
-    'Sashimi']
+import math
 
-}
+a = math.ceil(1.2)
 
-age = "18"
+print(a)
 
-# int_age = int(age)
-# print(int_age)
-# print(type(int_age))
 
-def my_function(who):
-    print('Hello! ', who)
-
-# my_function('Taehoon')
-
-def plus(a, b=0):
-    c = a + b
-
-    return c
-
-# print(plus(1))
-
-def plus(a=0, b=0):
-    c = a + b
-
-    return c
-
-# print(plus(1, 7))
-# print(plus())
 
 
 
